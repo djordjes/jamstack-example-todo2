cffaa71623041522dc8827794d1d841578c73623
Đorđe Savić <djordje.savic@tobonet.com>
2020-08-01 13:42:25 +0200

    revert client

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,6 +4,9 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
   uri: "https://graphql.fauna.com/graphql",
   cache: new InMemoryCache(),
+  headers: {
+    authorization: `Bearer ${process.env.FAUNA_SERVER_SECRET}`,
+  },
 });
 
 export const wrapRootElement = ({ element }) => (
