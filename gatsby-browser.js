ca1ee7fb1165ca88ec08b6e86f47ab613ce726e5
Đorđe Savić <djordje.savic@tobonet.com>
2020-08-01 16:37:11 +0200

    env fauna

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-console.log(process.env);
-
 const client = new ApolloClient({
   uri: "https://graphql.fauna.com/graphql",
   cache: new InMemoryCache(),
   headers: {
-    authorization: `Bearer ${build.env.FAUNADB_CLIENT_SECRET}`,
+    authorization: `Bearer ${process.env.FAUNADB_ADMIN_SECRET}`,
   },
 });
 
