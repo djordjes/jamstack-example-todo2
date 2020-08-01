2d75a627b45ffee5a249bec5649fa6240f10d139
Đorđe Savić <djordje.savic@tobonet.com>
2020-08-01 12:39:54 +0200

    example with apollo and fauna GraphQL

---
+++ gatsby-browser.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://graphql.fauna.com/graphql",
+  cache: new InMemoryCache(),
+  headers: {
+    authorization: `Bearer ${process.env.FAUNA_SERVER_SECRET}`,
+  },
+});
+
+export const wrapRootElement = ({ element }) => (
+  <ApolloProvider client={client}>{element}</ApolloProvider>
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,37 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
+
+const ALL_CUSTOMERS = gql`
+  {
+    allCustomers {
+      data {
+        firstName
+        lastName
+        _id
+      }
+    }
+  }
+`;
 
 export default () => {
+  const { loading, error, data } = useQuery(ALL_CUSTOMERS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  console.log(data);
+
   return (
     <>
       <h1>Hello Gatsby !</h1>
+      <ul>
+        {data.allCustomers.data.map((customer) => {
+          return (
+            <li key={customer._id}>
+              {customer._id} {customer.firstName} {customer.lastName}
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
