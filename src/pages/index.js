f9fefe52f8f5b6f155c791ceabeedb9b011c1c14
Đorđe Savić <djordje.savic@tobonet.com>
2020-08-01 13:38:08 +0200

    remove queries

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,11 @@
 import React from "react";
-import { useQuery, gql } from "@apollo/client";
-
-const ALL_CUSTOMERS = gql`
-  {
-    allCustomers {
-      data {
-        firstName
-        lastName
-        _id
-      }
-    }
-  }
-`;
 
 export default () => {
-  const { loading, error, data } = useQuery(ALL_CUSTOMERS);
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
-  console.log(data);
+  console.log(process.env);
 
   return (
     <>
       <h1>Hello Gatsby !</h1>
-      <ul>
-        {data.allCustomers.data.map((customer) => {
-          return (
-            <li key={customer._id}>
-              {customer._id} {customer.firstName} {customer.lastName}
-            </li>
-          );
-        })}
-      </ul>
     </>
   );
 };
